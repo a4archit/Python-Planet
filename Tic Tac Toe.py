1d443953813dcacda55c938c5f057ad51a5b8a11
Archit <archittyagi599@gmail.com>
2024-11-28 12:36:16 +0530

    Update Tic Tac Toe.py

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -4,26 +4,47 @@ import random
 class TicTacToe:
     
     def __init__(self):
-        self.matrix = np.full((3,3), '.', dtype=str)
+        self.matrix = np.full((3,3), None)
         
     def display(self):
         print()
         for row in self.matrix:
             for element in row:
-                print("\t", element, end='')
+                element_msg = 'X' if element == 1 else 'O' if element == 0 else '.'
+                print("\t", element_msg, end='')
             print()
         print()
+    
+    def get_numeric_matrix(self):
+        pass
         
     def set_value(self,player, r, c):
-        value = 'X' if player == 1 else 'O'
+        value = 1 if player == 1 else 0
         self.matrix[r,c] = value
     
     def get_blank_indices(self):
-        indices = np.array(np.where(self.matrix == '.'))
+        indices = np.array(np.where(self.matrix == None))
         processed_indices = []
         for i in range(len(indices[0])):
             processed_indices.append((indices[0][i], indices[1][i]))
         return processed_indices
+        
+    def check_winning(self):
+        check_winning_rows_()
+        
+    def check_winning_(self, action_on: str) -> str:
+        match(action_on):
+            case 'rows':
+                for row in self.matrix:
+                    if""
+            case 'columns':
+                pass
+            case 'diagnols':
+                pass
+            case _ :
+                raise('Invalid value of \'action_on\'')
+    
+    # --------- end of class 'TickTacToe' ------------ #
     
 def clean_user_choice(user_choice):
     return [int(element)-1 for element in user_choice.split(',')]
